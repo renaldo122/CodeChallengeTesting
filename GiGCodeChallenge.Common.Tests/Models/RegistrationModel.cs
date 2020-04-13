7511ac55b0438ea8ac5121d6e6be6665332aa901
rshehaj <rshehaj@revzonesolutions.com>
2020-04-13 23:27:20 +0200

    -Added Documentation

--- GiGCodeChallenge.Common.Tests/Models/RegistrationModel.cs
+++ GiGCodeChallenge.Common.Tests/Models/RegistrationModel.cs
@@ -4,8 +4,8 @@ namespace GiGCodeChallenge.Common.Tests.Models
 {
     public class RegistrationModel
     {
-        [JsonProperty("email")]
-        public string Email { get; set; }
+        [JsonProperty("email")] 
+        public string Email { get; set; } 
 
         [JsonProperty("password")]
         public string Password { get; set; }
